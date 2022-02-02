a5e1cacee196cb9c651afbfdac6be1d22cc4744a
Ismail <ismail.yimer.h@gmail.com>
2022-02-02 22:27:36 +0300

    fixed top header session

--- src/components/topHeader.php
+++ src/components/topHeader.php
@@ -1,3 +1,8 @@
+<?php
+if (isset($_SESSION['user'])) {
+    $user = unserialize($_SESSION['user']);
+}
+?>
 <!-- TOP HEADER -->
 <div id="top-header">
     <div class="container">
@@ -7,8 +12,48 @@
             <li><a href="#"><i class="fa fa-map-marker"></i>Addis Ababa, Ethiopia</a></li>
         </ul>
         <ul class="header-links pull-right">
-            <li><a href="#"><i class="fa fa-dollar"></i>BIRR</a></li>
-            <li><a href="/comart/src/routes/auth/signin.php"><i class="fa fa-user-o"></i> My Account</a></li>
+            <li>
+                <a href="
+                    <?php
+                    if (isset($user)) {
+                        switch ($user->getRole()) {
+                            case 'admin':
+                                echo '/comart/src/routes/dashboard/admin.php';
+                                break;
+                            case 'vendor':
+                                echo '/comart/src/routes/dashboard/vendor.php';
+                                break;
+                            case 'buyer':
+                                echo '/comart/index.php';
+                                break;
+                        }
+                    } else {
+                        echo '/comart/src/routes/auth/signin.php';
+                    }
+                    ?>
+                ">
+                    <i class="fa fa-user-o"></i>
+                    <?php
+                    if (isset($user)) {
+                        echo strtoupper($user->getUsername());
+                    } else {
+                        echo 'Sign In / Sign Up';
+                    }
+                    ?>
+                </a>
+            </li>'
+
+            <?php
+            if (isset($user)) {
+                echo
+                '<li>
+                    <a href="/comart/src/routes/auth/signout.php">
+                        <i class="fa fa-sign-out"></i>
+                        Sign Out
+                    </a>
+                </li>';
+            }
+            ?>
         </ul>
     </div>
 </div>
