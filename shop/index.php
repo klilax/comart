71868ab095aea967873d52ad70bf610b386ffc7f
Khalil Ahmed <klilax@hotmail.co.uk>
2022-02-05 11:38:34 +0300

    cart test

--- shop/index.php
+++ shop/index.php
@@ -1,6 +1,8 @@
 <?php
-require('../class/User.php');
-session_start();
+//require('../class/User.php');
+require('../class/Cart.php');
+require('../class/Inventory.php');
+//session_start();
 if (!isset($_SESSION['user'])) {
     header('location: ../signin.php');
 }
@@ -13,22 +15,75 @@ if (!isset($_SESSION['user'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <title>shop</title>
 </head>
 <body>
+
     <h1>Shopping</h1>
     <?php
         $buyer = unserialize($_SESSION['user']);
-        echo $buyer->getUsername()."<br>";
+        $buyerId = $buyer->getId();
+
+//        Cart::removeAllFromCart($buyerId);
+//        Cart::addToCart($buyerId,1,'RHS30X30x2' ,780.50, 7);
+//        Cart::addToCart($buyerId,7,'Rebar #3' ,550,14);
+//        Cart::addToCart($buyerId,13, 8);
+//        Cart::removeFromCart($buyerId, 1);
+
+//        echo $buyer->getUsername()."<br>";
+//        foreach ($_SESSION['cart'][$buyerId] as $key => $value){
+//            echo $key.'<br>';
+//            echo $_SESSION['cart'][$buyerId][$key]['name'].'<br>';
+//            echo $_SESSION['cart'][$buyerId][$key]['quantity'].'<br>';
+//            echo $value.'<br>';
+//        }
+
     ?>
-    <input type="text" onkeyup="searchItem()">
+    <input type="text" id="searchItem" onkeyup="searchItem()">
+    <table class="table table-dark table-striped" id="Tablecontent">
+        <thead>
+            <th>#</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Available Stock</th>
+        </thead>
+        <tbody>
+            <?php
+                foreach (Inventory::getAllInventory('all') as $row) {
+                    $inventoryId = $row['inventoryId'];
+                    $name = $row['inventoryName'];
+                    $price = $row['price'];
+                    $qty = $row['quantity'];
+                    echo '<tr>';
+                    echo '<td>'.$inventoryId.'</td>';
+                    echo '<td>'.$name.'</td>';
+                    echo '<td>'.$price.'</td>';
+                    echo '<td>'.$qty.'</td>';
+                    $encoded_name = urlencode($name);
+//                    echo '<td><a href="../class/Cart.php?inventoryId=$inventoryId&productName=$name&price=$price"><button type="button">Add to cart</button></a></td>';
+                    echo '<td><a href="../class/Cart.php?inventoryId='.$inventoryId.'&productName='.$encoded_name.'&price='.$price.'"><button type="button">Add to cart</button></a></td>';
+
+                }
+            ?>
+        </tbody>
+    </table>
 </body>
 <script>
-    const ajax = new XMLHttpRequest();
+
 
 
     function searchItem() {
-        console.log('hello');
+        let query = document.getElementById("searchItem").value;
+        const ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById("Tablecontent").innerHTML = this.responseText;
+            }
+        };
+        ajax.open("GET", "../class/searchInventory.php?query=" + query + "&category=all", true);
+        ajax.send();
+        console.log(query);
     }
 </script>
 </html>
