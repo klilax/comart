a5f3e8b5443f1c91b066666cef1d436547465c83
Ismail <ismail.yimer.h@gmail.com>
2022-02-06 10:46:07 +0300

    cart update

--- class/Cart.php
+++ class/Cart.php
@@ -3,19 +3,19 @@
 session_start();
 
 class Cart {
-    static function initCart($userId){
+    static function initCart($userId) {
         $_SESSION['cart'][$userId] = array();
     }
 
 
-    static function addToCart($userId, $inventoryId, $productName, $price, $quantity=1) {
+    static function addToCart($userId, $inventoryId, $productName, $price, $quantity = 1) {
         if (!isset($_SESSION['cart'][$userId])) {
             self::initCart($userId);
         }
         if (isset($_SESSION['cart'][$userId][$inventoryId])) {
             $_SESSION['cart'][$userId][$inventoryId]['quantity'] += $quantity;
         } else {
-            $_SESSION['cart'][$userId][$inventoryId]['quantity']= $quantity;
+            $_SESSION['cart'][$userId][$inventoryId]['quantity'] = $quantity;
         }
         $_SESSION['cart'][$userId][$inventoryId]['name'] = $productName;
         $_SESSION['cart'][$userId][$inventoryId]['price'] = $price;
@@ -33,20 +33,21 @@ class Cart {
         if (isset($_SESSION['cart'][$userId])) {
             foreach ($_SESSION['cart'][$userId] as $inventoryId => $quantity) {
                 echo "<tr>";
-                echo "<td>".$quantity['name']."</td>";
-                echo "<td>".$quantity."</td>";
-                echo "<td><a href='removeFromCart.php?userId=".$userId."&inventoryId=".$inventoryId."'>Remove</a></td>";
+                echo "<td>" . $quantity['name'] . "</td>";
+                echo "<td>" . $quantity . "</td>";
+                echo "<td><a href='removeFromCart.php?userId=" . $userId . "&inventoryId=" . $inventoryId . "'>Remove</a></td>";
                 echo "</tr>";
             }
         }
     }
 }
-$buyerId = $_SESSION['id'];
-if (isset($_GET['inventoryId'], $_GET['productName'], $_GET['price'])) {
-    $inventoryId = $_GET['inventoryId'];
-    $name = urldecode($_GET['productName']);
-    $price = $_GET['price'];
-    Cart::addToCart($buyerId, $inventoryId, $name, $price);
-    header("Location: ../shop/index.php");
+if (isset($_SESSION['id'])) {
+    if (isset($_GET['inventoryId'], $_GET['productName'], $_GET['price'])) {
+        $buyerId = $_SESSION['id'];
+        $inventoryId = $_GET['inventoryId'];
+        $name = urldecode($_GET['productName']);
+        $price = $_GET['price'];
+        Cart::addToCart($buyerId, $inventoryId, $name, $price);
+        header("Location: ../shop/index.php");
+    }
 }
-
