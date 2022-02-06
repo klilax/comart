db4e79291bcde6e8e995c9948a699b47f290840d
Mikias <mikias.worash.a@gmail.com>
2022-02-06 10:33:30 +0300

    main header

--- src/components/mainHeader.php
+++ src/components/mainHeader.php
@@ -1,3 +1,8 @@
+<?php
+    if (isset($_SESSION['user'])) {
+        $user = unserialize($_SESSION['user']);
+    }
+?>
 <!-- MAIN HEADER -->
 <div id="header">
     <!-- container -->
@@ -13,69 +18,93 @@
                 </div>
             </div>
             <!-- /LOGO -->
-
-            <!-- SEARCH BAR -->
-            <div class="col-md-6">
-                <div class="header-search">
-                    <form>
-                        <select class="input-select">
-                            <option value="0">All Categories</option>
-                            <option value="1">Category 01</option>
-                            <option value="1">Category 02</option>
-                        </select>
-                        <input class="input" placeholder="Search here">
-                        <button class="search-btn">Search</button>
-                    </form>
-                </div>
-            </div>
-            <!-- /SEARCH BAR -->
+        <!-- SEARCH BAR -->
+        <?php
+            if (!isset($user)) {
+                    echo '<div class="col-md-6">
+                        <div class="header-search">
+                            <form>
+                                <select class="input-select">
+                                    <option value="0">All Categories</option>
+                                    <option value="1">Category 01</option>
+                                    <option value="1">Category 02</option>
+                                </select>
+                                <input class="input" placeholder="Search here">
+                                <button class="search-btn">Search</button>
+                            </form>
+                        </div>
+                    </div>';
+            } else {
+                if ($user->getRole() == 'admin') {
+                    echo '<h1 style= "color: green;
+                        margin-top: 10px;
+                        margin-left: 15%;">
+                        Admin Dashboard
+                    </h1>';
+                } 
+                // else if ($user->getRole() == 'vendor') {
+                //     echo '<h1 style= "color: green;
+                //         margin-top: 10px;
+                //         margin-left: 15%;">
+                //         Vendor Dashboard
+                //     </h1>';
+                // }
+            }
+        ?>
+        <!-- /SEARCH BAR -->
+        
+           
 
             <!-- ACCOUNT -->
             <div class="col-md-3 clearfix">
                 <div class="header-ctn">
-
                     <!-- Cart -->
-                    <div class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                            <i class="fa fa-shopping-cart"></i>
-                            <span>Your Cart</span>
-                            <div class="qty">3</div>
-                        </a>
-                        <div class="cart-dropdown">
-                            <div class="cart-list">
-                                <div class="product-widget">
-                                    <div class="product-img">
-                                        <img src="../../img/product01.png" alt="">
+                <?php
+                    if(!isset($user) || $user->getRole() == 'buyer') {
+                        echo '
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span>Your Cart</span>
+                                <div class="qty">3</div>
+                            </a>
+                            <div class="cart-dropdown">
+                                <div class="cart-list">
+                                    <div class="product-widget">
+                                        <div class="product-img">
+                                            <img src="../../img/product01.png" alt="">
+                                        </div>
+                                        <div class="product-body">
+                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+                                        </div>
+                                        <button class="delete"><i class="fa fa-close"></i></button>
                                     </div>
-                                    <div class="product-body">
-                                        <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                        <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+    
+                                    <div class="product-widget">
+                                        <div class="product-img">
+                                            <img src="../../img/product02.png" alt="">
+                                        </div>
+                                        <div class="product-body">
+                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
+                                        </div>
+                                        <button class="delete"><i class="fa fa-close"></i></button>
                                     </div>
-                                    <button class="delete"><i class="fa fa-close"></i></button>
                                 </div>
-
-                                <div class="product-widget">
-                                    <div class="product-img">
-                                        <img src="../../img/product02.png" alt="">
-                                    </div>
-                                    <div class="product-body">
-                                        <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                        <h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
-                                    </div>
-                                    <button class="delete"><i class="fa fa-close"></i></button>
+                                <div class="cart-summary">
+                                    <small>3 Item(s) selected</small>
+                                    <h5>SUBTOTAL: $2940.00</h5>
+                                </div>
+                                <div class="cart-btns">
+                                    <a href="#">View Cart</a>
+                                    <a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
-                            <div class="cart-summary">
-                                <small>3 Item(s) selected</small>
-                                <h5>SUBTOTAL: $2940.00</h5>
-                            </div>
-                            <div class="cart-btns">
-                                <a href="#">View Cart</a>
-                                <a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- /Cart -->
+                        </div>';
+                    }
+                ?>
+                <!-- /Cart -->
 
                     <!-- Menu Toogle -->
                     <div class="menu-toggle">
