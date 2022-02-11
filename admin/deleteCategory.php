354f48c856021bd8da02721b88a478be06c7e4d6
Mikias <mikias.worash.a@gmail.com>
2022-02-11 23:02:40 +0300

    category update

---
+++ admin/deleteCategory.php
@@ -0,0 +1,27 @@
+<?php
+require('../class/User.php');
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('location: ../signin.php');
+}
+if ($_SESSION['role'] != 'admin') {
+    header('location: ../signin.php');
+}
+
+$categoryName = $_GET['id'];
+
+$sql = "DELETE FROM category WHERE categoryName = :categoryName";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':categoryName', $categoryName);
+$stmt->execute();
+
+if ($stmt->rowCount() == 1) {
+    $message = "Category deleted.";
+    $opeStatus = 0;
+} else {
+    $message = "Error in deleting Category, Try again.";
+    $opeStatus = 1;
+}
+$_SESSION['message'] = $message;
+$_SESSION['opeStatus'] = $opeStatus;
+header("Location: index.php");
