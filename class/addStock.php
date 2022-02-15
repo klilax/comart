10ebd5e5c5f327368aa1592ab5bde1d3065b683c
Khalil Ahmed <klilax@hotmail.co.uk>
2022-02-15 14:29:43 +0300

    routing fix

--- class/addStock.php
+++ class/addStock.php
@@ -5,8 +5,6 @@ session_start();
 if (isset($_GET['inventoryId'], $_GET['quantity'])) {
     $inventoryId = $_GET['inventoryId'];
     $quantity = $_GET['quantity'];
-    echo $inventoryId;
-    echo $quantity;
     Inventory::addStock($inventoryId, $quantity);
     $_SESSION['adjust_success_1'] = "Added stock successfully";
     header('location: ../vendor/index.php');
