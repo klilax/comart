8b4e6616e1b6c329ea377bc7f161fb4ecb4ca8e9
Ismail <ismail.yimer.h@gmail.com>
2022-02-17 16:40:06 +0300

    updated footer and others

--- js/menu-toggle.js
+++
@@ -1,7 +0,0 @@
-const menu = document.getElementsByClassName('menu-toggle')[0];
-// console.log(menu);
-menu.addEventListener('click', (e) => {
-    let icon = e.target.classList;
-    icon.toggle('fa-close');
-    icon.toggle('fa-bars');
-})
--- src/components/footer.php
+++ src/components/footer.php
@@ -70,12 +70,7 @@
             <div class="row">
                 <div class="col-md-12 text-center">
                     <ul class="footer-payments">
-                        <li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
-                        <li><a href="#"><i class="fa fa-credit-card"></i></a></li>
-                        <li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
-                        <li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
-                        <li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
-                        <li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+                        <li>comart&copy;2022 | All rights reserved.</li>
                     </ul>
                 </div>
             </div>
